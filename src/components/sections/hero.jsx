031de7543add2557687afbdfc944322808e12f15
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-07-18 16:07:15 +0600

    fixes, add favicon, styling hero section

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -7,9 +7,18 @@ function hero() {
 		<section className="hero">
 			<img className="hero__rings" src={rings} alt="rings"/>
 			<div className="container">
+				<div className="hero__content">
+					<h1 className="hero__title">Light a fire under your taste buds</h1>
+					<h3 className="hero__subtitle">Nacho cheese</h3>
+					<p className="hero__description">Corn, Vegetable Oil (Corn, Canola, And/or Sunflower Oil), Maltodextrin (Made From Corn), Salt, Cheddar Cheese (Milk, Cheese Cultures, Salt, Enzymes), Whey, Monosodium Glutamate, Buttermilk, Romano Cheese (Part-skim Cow's Milk, Cheese Cultures, Salt, Enzymes), Whey Protein Concentrate, Onion Powder, Corn Flour, Natural And Artificial Flavor, Dextrose, Tomato Powder, Lactose, Spices, Artificial Color (Yellow 6, Yellow 5, And Red 40), Lactic Acid, Citric Acid, Sugar, Garlic Powder, Skim Milk, Red And Green Bell Pepper Powder, Disodium Inosinate, And Disodium Guanylate.
+						<strong>CONTAINS MILK INGREDIENTS.</strong>
+					</p>
+					<h2 className="hero__price">$3.99</h2>
+					<a className="hero__btn" href="">Add to cart</a>
+				</div>
 			</div>
 		</section>
 	)
 }
 
-export default hero
+export default hero;
