53e8de857cac2a90509fb12df7a9cac0f22f4791
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-07-18 15:20:28 +0600

    end up header.jsx

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -4,12 +4,11 @@ import rings from '../../assets/rings.png'
 
 function hero() {
 	return (
-		<div className="hero">
+		<section className="hero">
 			<img className="hero__rings" src={rings} alt="rings"/>
 			<div className="container">
-				
 			</div>
-		</div>
+		</section>
 	)
 }
 
