354afa2d7e161083ed6965809c182b95fe5cacf7
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-07-19 01:45:15 +0600

    build for gh-pages

---
+++ precache-manifest.0f40e2df0d22212ee72591446619ee18.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd35341fc8f6456fee94764ad5530938",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc99e7c74dd3b5e8b6f4",
+    "url": "./static/css/main.232782e0.chunk.css"
+  },
+  {
+    "revision": "b9439b83cd91b3997877",
+    "url": "./static/js/2.f2bfc111.chunk.js"
+  },
+  {
+    "revision": "3082c663d739339f4b31702ae323437e",
+    "url": "./static/js/2.f2bfc111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc99e7c74dd3b5e8b6f4",
+    "url": "./static/js/main.c250070c.chunk.js"
+  },
+  {
+    "revision": "7ea3c3e34e2aecd2ab1e",
+    "url": "./static/js/runtime-main.4f26c735.js"
+  },
+  {
+    "revision": "dc0d4b907c5f90bc0b07e6b1bb653717",
+    "url": "./static/media/cart.dc0d4b90.svg"
+  },
+  {
+    "revision": "645f564f53623dbabc1a7af96549aae8",
+    "url": "./static/media/chips.645f564f.png"
+  },
+  {
+    "revision": "65bd42f991dfcdc0630dd16d0de8894b",
+    "url": "./static/media/doritos.65bd42f9.png"
+  },
+  {
+    "revision": "18b09ee64f47d03f35eacf684a12fe0b",
+    "url": "./static/media/doritos_logo.18b09ee6.png"
+  },
+  {
+    "revision": "3715f3bccd0bbc8d3327ba338b367855",
+    "url": "./static/media/ketchup.3715f3bc.png"
+  },
+  {
+    "revision": "5da357da6c1cc86fb08c15eba3dfb114",
+    "url": "./static/media/rings.5da357da.png"
+  },
+  {
+    "revision": "07c5bdc5209752a1ec559ce0b7aa5b36",
+    "url": "./static/media/suyaPepper.07c5bdc5.png"
+  }
+]);
